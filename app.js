c087c968eba889e8630b76ee3a9e00a3c026d198
Raphael <95745431+lemrex@users.noreply.github.com>
2024-08-13 18:30:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('Hello, World Lagos!');
+  res.send('Hello, World Lag!');
 });
 
 // Start the server
